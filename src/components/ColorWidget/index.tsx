5834ca43647ebaf1f18737e9da0113f648129e0a
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-05-29 19:14:51 -0300

    🔨 Remove unused files and update extensions to *.ts,*.tsx

---
+++ src/components/ColorWidget/index.tsx
@@ -0,0 +1,28 @@
+import ColorSelector from "./ColorSelector";
+import ColorWindow from "./ColorWindow";
+
+import "./index.css";
+
+type ColorWidgetProps = {
+  colorList: string[];
+};
+
+const ColorWidget = ({ colorList }: ColorWidgetProps) => {
+  console.log(colorList);
+
+  return (
+    <div className="color-widget">
+      <div className="color-widget-title">
+        <h2>I am a placeholder</h2>
+      </div>
+      <div>
+
+          <ColorWindow />
+          <ColorSelector />
+
+      </div>
+    </div>
+  );
+};
+
+export default ColorWidget;
