347cb84254b003197e740586c0c2a18ecd2264d2
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-05-30 01:51:07 -0300

    ✨ Add `api` folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
 import styled from 'styled-components';
 
+import { useGetColors } from 'src/api/colors';
 import ColorWidget from 'src/components/ColorWidget';
-
-const COLOR_LIST = [
-  '#D7CEE4',
-  '#CFB4F7',
-  '#6221EA',
-  '#0725E8',
-  '#B6BFF6',
-  '#FEBECD',
-  '#FBC02D',
-  '#109CF1',
-  '#FF2455',
-  '#FF8A00',
-  '#8F40EB',
-  '#AF5EEC',
-  '#CA7DEF',
-  '#DF9CF3',
-  '#F1BCF8',
-  '#FFDCFF',
-  '#FBC4ED',
-  '#F8ABD6',
-  '#F591BC',
-  '#F1779E',
-  '#E95D7E',
-  '#DE425B',
-  '#FF005B',
-  '#FF533B',
-  '#FF811D',
-  '#FFA600',
-];
 
 const AppContainer = styled.div`
   width: 100wh;
@@ -55,10 +27,14 @@ const AppContainer = styled.div`
   );
 `;
 
-const App = () => (
-  <AppContainer>
-    <ColorWidget colors={COLOR_LIST} />
-  </AppContainer>
-);
+const App = () => {
+  const { data: colors } = useGetColors();
+
+  return (
+    <AppContainer>
+      <ColorWidget colors={colors} />
+    </AppContainer>
+  );
+};
 
 export default App;
